dcdfe3b2a559f9d86bade0e34a5220eb93508668
krish-ktm <ktpatel99@gmail.com>
2025-01-24 19:02:58 +0530

    import & export

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {CardCompo} from "./components/CardCompo";
 
 export const App = () => {
   return (
@@ -12,28 +13,3 @@ export const App = () => {
   )
 };
 
-
-const CardCompo = () => {
-
-  const name = "Jemmy Patel";
-  const subTxt = "Papqnclk ns;alfknvskdvljn";
-
-  const ParaFunc = () => {
-    const para = "oaslkjfaolknv l;jsbnlvidskbvnlkj.bjnasiluvbkn lkjaniwfvbuen il;l";
-    return para
-  };
-
-  return (
-    <div>
-
-      <h2>{name}</h2>
-
-      <h3>{subTxt}</h3>
-
-      <p>{ParaFunc()}</p>
-
-
-    </div>
-
-  )
-}
---
+++ src/components/CardCompo.jsx
@@ -0,0 +1,29 @@
+
+export const CardCompo = () => {
+
+    const name = "Jemmy Patel";
+    const subTxt = "Papqnclk ns;alfknvskdvljn";
+
+    const ParaFunc = () => {
+        const para = "oaslkjfaolknv l;jsbnlvidskbvnlkj.bjnasiluvbkn lkjaniwfvbuen il;l";
+        return para
+    };
+
+    let age = 15;
+
+
+    return (
+        <div>
+
+            <h2>{name}</h2>
+
+            <h3>{subTxt}</h3>
+
+            <p>{ParaFunc()}</p>
+
+            {age >= 20 && <button>Watch Now</button>}
+
+        </div>
+
+    )
+}
